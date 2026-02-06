cd2607b59e5b8f802a3b11044b40fcff76aa31e1
ranjthreddy <anthapuranjithreddy69@gmail.com>
2026-02-06 12:36:49 +0530

    backend

--- backend/backend/router/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const {getUser,getUserById,createUser,editUser,deleteUser}
-const userRoute=express.Router();
-userRoute.get("/",getUser);
-userRoute.get("/:id",getUserById);
-userRoute.post("/create",createUser);
-userRoute.put("/update/:id",editUser);
-userRoute.delete("/delete",deleteUser);
-module.exports=userRoute;
